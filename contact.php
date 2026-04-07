e67dc74ffd1f13a0ba495600b60d87d5fb42686b
xobazjr <MR.TeerapatKotanartChannel@Gmail.COM>
2026-04-07 11:37:38 +0700

    Update about form
    ✅ Update form connect to php file

--- contact.php
+++ contact.php
@@ -6,6 +6,19 @@
     <title>Contact</title>
 </head>
 <body>
-    
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $company = $_POST["fcompany"];
+            $phone = $_POST["fphone"];
+            $name = $_POST["fname"];
+
+            echo "<table>"."<br>";
+            echo "<tr>"."<br>";
+            echo "<th>"."Company";
+            echo "</tr>"."<br>";
+            echo "</table>";
+
+        }
+    ?>
 </body>
 </html>
